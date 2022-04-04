d6279ea024b43bb07341eddfaf6dddb194ec0269
Menerv <mail@menerv.net>
2022-04-04 15:27:43 +0200

    Remove private server dependancy

--- src/scripts/r-row/network/socket.ts
+++ src/scripts/r-row/network/socket.ts
@@ -1,7 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket = null;
-let clients: any = {};
 let serverURL: string = null;
 
 export const setServerURL = (url: string) => {
@@ -10,17 +9,8 @@ export const setServerURL = (url: string) => {
 
 export const getSocket = () => {
   if (serverURL === null) throw new Error("No server URL provided");
-  if (socket === null) {
-    socket = io(serverURL);
-    socket.on("clients", (c) => {
-      clients = c;
-    });
-  }
+  if (socket === null) socket = io(serverURL);
   return socket;
-};
-
-export const getClients = () => {
-  return clients;
 };
 
 export const isServerConnected = () => {
