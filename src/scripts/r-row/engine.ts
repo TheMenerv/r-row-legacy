d5a4e804abb7416c2f7e6441b9c6301f09c61938
Menerv <mail@menerv.net>
2023-02-18 12:26:46 +0100

    Fix no sound on iOS and iPadOS devices

--- src/scripts/r-row/engine.ts
+++ src/scripts/r-row/engine.ts
@@ -1,7 +1,8 @@
-import { logger } from "./logger";
-import { getCanvas } from "./canvas";
-import { startGameLoop } from "./gameLoop";
-import { load, main } from "..";
+import { logger } from './logger';
+import { getCanvas } from './canvas';
+import { startGameLoop } from './gameLoop';
+import { load, main } from '..';
+import { getStore } from './store';
 
 const logo = `    Powered by
         ██████╗       ██████╗  ██████╗ ██╗    ██╗  █╗
@@ -15,9 +16,31 @@ const logo = `    Powered by
 const run = async () => {
   logger.info(logo);
   await load();
+  iOsUnlockSound();
   getCanvas();
   startGameLoop();
   main();
 };
 
 run();
+
+const iOsUnlockSound = () => {
+  document.body.addEventListener(
+    'touchstart',
+    () => {
+      if (audiosToUnlock) {
+        for (let audio of audiosToUnlock) {
+          audio.play();
+          audio.pause();
+          audio.currentTime = 0;
+        }
+        audiosToUnlock = [];
+      }
+    },
+    false
+  );
+  let audiosToUnlock: HTMLAudioElement[] = [];
+  Object.entries(getStore().sounds).forEach(([_, sound]) => {
+    audiosToUnlock.push(sound);
+  });
+};
