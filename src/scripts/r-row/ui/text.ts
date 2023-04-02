ae43236633f5d18d09a54d01df8ca216c93d21f6
Menerv <mail@menerv.net>
2023-04-03 01:22:19 +0200

    v1.2.18 Add stroke capability to drawText()

--- src/scripts/r-row/ui/text.ts
+++ src/scripts/r-row/ui/text.ts
@@ -4,19 +4,26 @@ export const drawText = (
   x: number,
   y: number,
   color?: string,
-  position: "right" | "center" | "left" = "center",
+  position: 'right' | 'center' | 'left' = 'center',
   font?: {
     name: string;
     size: string;
-  }
+  },
+  strokeColor?: string,
+  strokeSize?: number
 ) => {
   ctx.save();
   if (color !== undefined) ctx.fillStyle = color;
   if (font !== undefined) ctx.font = `${font.size} ${font.name}`;
   const textWidth = ctx.measureText(text).width;
   let posX = x - textWidth / 2;
-  if (position === "left") posX = x;
-  else if (position === "right") posX = x - textWidth;
+  if (position === 'left') posX = x;
+  else if (position === 'right') posX = x - textWidth;
+  if (strokeColor && strokeSize) {
+    ctx.strokeStyle = strokeColor;
+    ctx.lineWidth = strokeSize;
+    ctx.strokeText(text, posX, y);
+  }
   ctx.fillText(text, posX, y);
   ctx.restore();
 };
